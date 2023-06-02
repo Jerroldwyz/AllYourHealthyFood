d53b7849569505845bf83731c89d6d9ea68735d3
josialeho <62127435+josialeho@users.noreply.github.com>
2023-06-02 15:44:50 +1000

    styling
    abit

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import userAccount from './backend/UserAccount';
 function Home() {
   return (
     <div>
-      <h1>Product Catalogue</h1>
+      <h1 class="text-center align-middle py-8 font-bold text-4xl">Product Catalogue</h1>
       <div className="product-list">
         {userAccount.getCatalogue().map((product) => (
           <div className="product" key={product.id}>
@@ -15,9 +15,9 @@ function Home() {
                 style={{ backgroundImage: `url(${product.image})` }}
               ></div>
               <div className="product-details">
-                <p className="product-name">{product.name} - </p>
+                <p className="product-name">{product.name}</p>
                 <p className="product-price">${product.price}</p>
-                <p className="product-rating">- rating: {product.rating}</p>
+                <p className="product-rating">rating: {product.rating}</p>
               </div>
             </Link>
           </div>
